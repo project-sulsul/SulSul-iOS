f3498a0f3c57945a2d03a62e63fe5c332940cef4
Leebumju <dlqkawns@naver.com>
2023-11-20 00:17:18 +0900

    chore: 취향등록: 주종 선택 화면 파일 추가

---
+++ Projects/Feature/Sources/DrinkingRegister/DrinkingRegisterViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DrinkingRegisterViewController.swift
+//  Feature
+//
+//  Created by 이범준 on 2023/11/20.
+//
+
+import UIKit
+import DesignSystem
+
+class DrinkingRegisterViewController: BaseViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple400
+    }
+    public override func addViews() {
+        
+    }
+    public override func makeConstraints() {
+
+    }
+    public override func setupIfNeeded() {
+        
+    }
+}
+
